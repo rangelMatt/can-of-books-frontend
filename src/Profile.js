54009115397bba70ffd65a5e8c8c07c76f0162b0
Matt Rangel <95889943+rangelMatt@users.noreply.github.com>
2022-03-01 14:57:11 -0800

    Passed user information props

--- src/Profile.js
+++ src/Profile.js
@@ -5,8 +5,13 @@ class Profile extends Component {
   render() {
     /* TODO: render information about logged in user */
     /* STRETCH TODO: if no logged in user then redirect home */
-    return <p>Profile page coming soon</p>
-  }
-};
+    return (<>
+      {
+        this.props.user ?
+          (<p>{this.props.user.name}: {this.props.user.email}</p>) :
+          null
+      }</>)
+  };
+}
 
 export default Profile;
